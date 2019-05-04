c5c51f74d2056a5a8588f274a1c62f8a90e2e80c
Ashley <ashley@sa-irc.com>
2019-05-04 21:44:29 +0100

    Add mysql dump function

--- backup.sh
+++ backup.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+#
+# Copyright San Andreas IRC Network
+# All Rights Reserved
+#
+
+function printHelp() {
+    echo "Usage: "
+}
+
+function dumpMysql() {
+    SERVER=$1
+    PORT=$2
+    KEY=$3
+    PWD=$4
+
+    ssh -p $PORT -i $KEY root@$SERVER "mysqldump --password=${PWD} -A > /root/mysql/databases.sql"
+}
+
+function rsync() {
+    SERVER=$1
+    PORT=$2
+    KEY=$3
+}
+
+# Parse commandline args
+MODE=$1
+shift
+
+if [ "$MODE" == "cron" ]; then
+else
+    printHelp
+    exit 1
+fi
